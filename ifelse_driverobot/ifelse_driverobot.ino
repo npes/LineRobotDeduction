d6cd0aef07775140aa9e28aae7f5d27ccabd685c
thomasHSoerensen <the_dk_warlock@hotmail.com>
2015-12-17 12:33:47 +0100

    ifelse_driverobot_v1.2
    early version

--- ifelse_driverobot/ifelse_driverobot.ino
+++ ifelse_driverobot/ifelse_driverobot.ino
@@ -1,77 +1,38 @@
-int leftSensor=7;
-int rightSensor=8;
-int motorLeftA=6;
-int motorRightA=10;
-int motorLeftB=9;
-int motorRightB=3;
-int motorLeftSpeed=5;
-int motorRightSpeed=11;
+#include <TimerOne.h>
 
-void setup() {
-  // put your setup code here, to run once:
-pinMode (leftSensor, INPUT);
-pinMode (rightSensor, INPUT);
-pinMode (motorLeftA, OUTPUT);
-pinMode (motorRightA, OUTPUT);
-pinMode (motorLeftB, OUTPUT);
-pinMode (motorRightB, OUTPUT);
-pinMode (motorLeftSpeed, OUTPUT);
-pinMode (motorRightSpeed, OUTPUT);
+// This example creates a PWM signal with 25 kHz carrier.
+//
+// Arduino's analogWrite() gives you PWM output, but no control over the
+// carrier frequency.  The default frequency is low, typically 490 or
+// 3920 Hz.  Sometimes you may need a faster carrier frequency.
+//
+// The specification for 4-wire PWM fans recommends a 25 kHz frequency
+// and allows 21 to 28 kHz.  The default from analogWrite() might work
+// with some fans, but to follow the specification we need 25 kHz.
+//
+// http://www.formfactors.org/developer/specs/REV1_2_Public.pdf
+//
+// Connect the PWM pin to the fan's control wire (usually blue).  The
+// board's ground must be connected to the fan's ground, and the fan
+// needs +12 volt power from the computer or a separate power supply.
+
+const int fanPin = 4;
+
+void setup(void)
+{
+  Timer1.initialize(40);  // 40 us = 25 kHz
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-int stateLeftSensor=digitalRead (leftSensor);
-int stateRightSensor=digitalRead (rightSensor);
-//delay(5);
-int spd1=120; //turn speed fast wheel
-int spd2=150; //forward speed
-int spd3=0; //turn speed slow wheel
-int spd4=100; //reverse speed
-
-if (stateLeftSensor==LOW &&  stateRightSensor==HIGH) {
-
-  analogWrite(motorLeftSpeed, spd1);
-  analogWrite(motorRightSpeed, spd3);
-
-  digitalWrite(motorLeftA, HIGH);
-  digitalWrite(motorLeftB, LOW);
-
-  digitalWrite(motorRightA, HIGH);
-  digitalWrite(motorRightB, LOW);
+void loop(void)
+{
+  // slowly increase the PWM fan speed
+  //
+  for (float dutyCycle = 30.0; dutyCycle < 100.0; dutyCycle++) {
+    Serial.print("PWM Fan, Duty Cycle = ");
+    Serial.println(dutyCycle);
+    Timer1.pwm(fanPin, (dutyCycle / 100) * 1023);
+    delay(500);
+  }
 }
-else if (stateLeftSensor==HIGH &&  stateRightSensor==LOW){
 
-  analogWrite(motorLeftSpeed, spd3);
-  analogWrite(motorRightSpeed, spd1);
-
-  digitalWrite(motorLeftA, HIGH);
-  digitalWrite(motorLeftB, LOW);
-
-  digitalWrite(motorRightA, HIGH);
-  digitalWrite(motorRightB, LOW);
-}
-else if (stateLeftSensor==HIGH &&  stateRightSensor==HIGH){
-
-  analogWrite(motorLeftSpeed, spd2);
-  analogWrite(motorRightSpeed, spd2);
-
-  digitalWrite(motorLeftA, HIGH);
-  digitalWrite(motorLeftB, LOW);
-
-  digitalWrite(motorRightA, HIGH);
-  digitalWrite(motorRightB, LOW);
-}
-else if (stateLeftSensor==LOW &&  stateRightSensor==LOW){
-
-  analogWrite(motorLeftSpeed, spd4);
-  analogWrite(motorRightSpeed, spd4);
-
-  digitalWrite(motorLeftA, LOW);
-  digitalWrite(motorLeftB, HIGH);
-
-  digitalWrite(motorRightA, LOW);
-  digitalWrite(motorRightB, HIGH);
-//  delay (200);
-}
-}
